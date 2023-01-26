0349e17b8df8615559e50873897ebfa814aaa828
AlinaS13 <soulofangel13@gmail.com>
2023-01-27 00:16:19 +0200

    add hw-7

---
+++ js/01-gallery.js
@@ -0,0 +1,44 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const gallery = document.querySelector(".gallery");
+
+const createGalleryMarkup = galleryItems
+  .map(
+    ({
+      preview,
+      original,
+      description,
+    }) => `<a class="gallery__link" href="${original}"><img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    /></a>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", createGalleryMarkup);
+
+gallery.addEventListener("click", openModel);
+
+function openModel(event) {
+  event.preventDefault();
+
+  if (event.target.tagName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+        <img src="${event.target.getAttribute(
+          "data-source"
+        )}" width="800" height="600">
+    `);
+  instance.show();
+
+  gallery.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
+}
